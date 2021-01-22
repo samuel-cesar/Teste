c79847d5220373b54cd602672bcfdbaa1e841432
samuelssaquino <samuelssaquino@outlook.com>
2021-01-22 16:40:49 -0300

    Fourth commit

--- src/Teste.java
+++ src/Teste.java
@@ -2,5 +2,6 @@ public class Teste {
     public static void main(String[] args) {
         System.out.println("Teste OK!!");
         System.out.println("Commit 3");
+        System.out.println("Commit 4");
     }
 }
